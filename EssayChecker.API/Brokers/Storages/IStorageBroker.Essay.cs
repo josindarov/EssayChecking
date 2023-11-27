a42f92fd2fd46f9c1b07c4454aad01bc5ce46a0f
Jurabek <jorabeksindarov44@gmail.com>
2023-11-27 21:31:54 +0500

    BROKERS: IStorageBrokers methods added

--- EssayChecker.API/Brokers/Storages/IStorageBroker.Essay.cs
+++ EssayChecker.API/Brokers/Storages/IStorageBroker.Essay.cs
@@ -1,6 +1,14 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EssayChecker.API.Models.Foundation.Essays;
+
 namespace EssayChecker.API.Brokers.Storages;
 
 public partial interface IStorageBroker
 {
+    ValueTask<Essay> InsertEssayAsync(Essay essay);
+    ValueTask<Essay> SelectEssayByIdAsync(Guid id);
+    IQueryable<Essay> SelectAllEssays();
     
 }
---
+++ EssayChecker.API/Brokers/Storages/IStorageBroker.User.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+using System.Threading.Tasks;
+using EssayChecker.API.Models.Foundation.Users;
+
+namespace EssayChecker.API.Brokers.Storages;
+
+public partial interface IStorageBroker
+{
+    ValueTask<User> InsertUserAsync(User user);
+    ValueTask<User> SelectUserByIdAsync(User user);
+    IQueryable<User> SelectAllUsers();
+    ValueTask<User> UpdateUserAsync(User user);
+    ValueTask<User> DeleteUserAsync();
+}
