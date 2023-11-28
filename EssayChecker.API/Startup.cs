182d89af8caebd1123c5249412f94d89daf93a15
Jurabek <jorabeksindarov44@gmail.com>
2023-11-28 17:15:55 +0500

    INFRA: EssayChecker.API.Unit.Test project initialized

--- EssayChecker.API/Startup.cs
+++ EssayChecker.API/Startup.cs
@@ -1,4 +1,5 @@
 using EssayChecker.API.Brokers.Storages;
+using EssayChecker.API.Services.Foundation.Users;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
