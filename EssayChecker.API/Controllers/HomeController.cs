66b31e191f0f3108a1d07f8502a5cb665c60fac6
Jurabek <jorabeksindarov44@gmail.com>
2023-12-12 21:14:12 +0500

    Home controller added

---
+++ EssayChecker.API/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using EssayChecker.API.Brokers.OpenAI;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using Standard.AI.OpenAI.Models.Services.Foundations.ChatCompletions;
+using System.Threading.Tasks;
+
+namespace EssayChecker.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        private readonly IOpenAIBroker openAIBroker;
+
+        public HomeController(IOpenAIBroker openAIBroker)
+        {
+            this.openAIBroker = openAIBroker;
+        }
+
+        [HttpGet]
+        public ActionResult<string> GetHomeMessage() => 
+            Ok("Tarteeb is running");
+
+        [HttpPost]
+        public async ValueTask<ActionResult<ChatCompletion>> Post(
+            ChatCompletion chatCompletion)
+        {
+            return await openAIBroker
+                .AnalyseEssayAsync(chatCompletion); 
+        }
+
+    }
+}
