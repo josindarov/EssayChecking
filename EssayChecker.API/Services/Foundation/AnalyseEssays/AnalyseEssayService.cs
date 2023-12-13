3f32d78572faa8a01e3f671d91b1390950c251fe
Jurabek <jorabeksindarov44@gmail.com>
2023-12-13 14:50:58 +0500

    ShouldAnalyseEssayAsync -> PASS

--- EssayChecker.API/Services/Foundation/AnalyseEssays/AnalyseEssayService.cs
+++ EssayChecker.API/Services/Foundation/AnalyseEssays/AnalyseEssayService.cs
@@ -1,6 +1,8 @@
+using System.Linq;
 using System.Threading.Tasks;
 using EssayChecker.API.Brokers.Loggings;
 using EssayChecker.API.Brokers.OpenAIs;
+using Standard.AI.OpenAI.Models.Services.Foundations.ChatCompletions;
 
 namespace EssayChecker.API.Services.Foundation.AnalyseEssays;
 
@@ -18,6 +20,39 @@ public class AnalyseEssayService : IAnalyseEssayService
     
     public async ValueTask<string> AnalyseEssayAsync(string essay)
     {
-        throw new System.NotImplementedException();
+        ChatCompletion request = CreateRequest(essay);
+        
+        ChatCompletion response = await this.openAiBroker
+            .AnalyseEssayAsync(request);
+
+        return response.Response.Choices.FirstOrDefault().Message.Content;
+    }
+
+    private static ChatCompletion CreateRequest(string essay)
+    {
+        var request = new ChatCompletion
+        {
+            Request = new ChatCompletionRequest
+            {
+                Model = "gpt-3.5-turbo",
+                Messages = new ChatCompletionMessage[]
+                {
+                    new ChatCompletionMessage
+                    {
+                        Role = "System",
+                        Content =
+                            "You are IELTS Writing examiner. Give detailed IELTS score based on marking criteria of IELTS."
+                    },
+                    new ChatCompletionMessage
+                    {
+                        Role = "User",
+                        Content = essay
+                    }
+                }
+            }
+        };
+		
+        return request;
+
     }
 }
